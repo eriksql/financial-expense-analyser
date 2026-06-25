b55bde35d8c1699f39756b25be5797b9076d5442
Erik Carvalho <erikcarvalho40974097@gmail.com>
2026-06-25 16:54:21 -0300

    initial commit - Financial Expense Analyser

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load CSV file
+df = pd.read_csv("../data/expenses.csv")
+
+print("FINANCIAL EXPENSE ANALYZER\n")
+
+# Total expenses
+total_expenses = df["amount"].sum()
+print(f"Total Expenses: ${total_expenses:.2f}")
+
+# Average expense
+average_expense = df["amount"].mean()
+print(f"Average Expense: ${average_expense:.2f}")
+
+# Expenses by category
+category_totals = df.groupby("category")["amount"].sum()
+
+print("\nExpenses by Category:")
+print(category_totals)
+
+# Highest category
+highest_category = category_totals.idxmax()
+
+print(f"\nHighest Category: {highest_category}")
+
+# Create chart
+category_totals.plot(kind="bar")
+
+plt.title("Expenses by Category")
+plt.ylabel("Amount ($)")
+plt.tight_layout()
+
+plt.show()
